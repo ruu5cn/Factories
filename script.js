6ca0eed145ab5ddb38aae1590d139ba83da21ec7
Steven Copeland <26176516+StevenCopeland@users.noreply.github.com>
2018-01-25 10:54:06 -0500

    Adding basic js setup

---
+++ script.js
@@ -0,0 +1,13 @@
+// TODO: Add 2-6 players
+// TODO: Add computer generated players
+// TODO: Add basic code
+
+function player(name) {
+  this.name = name;
+  this.money = 15;
+  this.properties = [];
+}
+
+var bidding = function() {
+
+}
